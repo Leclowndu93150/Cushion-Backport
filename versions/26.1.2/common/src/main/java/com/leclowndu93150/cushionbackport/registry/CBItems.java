831c71bbf170df2b0bb93df3a31fdc7dc10cca5c
Leclowndu93150 <xdr.a123454321@gmail.com>
2026-07-08 23:45:33 +0200

    add support for dye depot

--- versions/26.1.2/common/src/main/java/com/leclowndu93150/cushionbackport/registry/CBItems.java
+++ versions/26.1.2/common/src/main/java/com/leclowndu93150/cushionbackport/registry/CBItems.java
@@ -1,42 +1,34 @@
 package com.leclowndu93150.cushionbackport.registry;
 
 import com.leclowndu93150.cushionbackport.item.CushionItem;
-import java.util.EnumMap;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Supplier;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 
 public class CBItems {
-    public static final Map<DyeColor, Supplier<CushionItem>> CUSHIONS = new EnumMap<>(DyeColor.class);
+    public static final Map<DyeColor, Supplier<CushionItem>> CUSHIONS = new HashMap<>();
 
-    private static final Map<DyeColor, Block> WOOL_BY_COLOR = new EnumMap<>(DyeColor.class);
-
-    static {
-        WOOL_BY_COLOR.put(DyeColor.WHITE, Blocks.WHITE_WOOL);
-        WOOL_BY_COLOR.put(DyeColor.ORANGE, Blocks.ORANGE_WOOL);
-        WOOL_BY_COLOR.put(DyeColor.MAGENTA, Blocks.MAGENTA_WOOL);
-        WOOL_BY_COLOR.put(DyeColor.LIGHT_BLUE, Blocks.LIGHT_BLUE_WOOL);
-        WOOL_BY_COLOR.put(DyeColor.YELLOW, Blocks.YELLOW_WOOL);
-        WOOL_BY_COLOR.put(DyeColor.LIME, Blocks.LIME_WOOL);
-        WOOL_BY_COLOR.put(DyeColor.PINK, Blocks.PINK_WOOL);
-        WOOL_BY_COLOR.put(DyeColor.GRAY, Blocks.GRAY_WOOL);
-        WOOL_BY_COLOR.put(DyeColor.LIGHT_GRAY, Blocks.LIGHT_GRAY_WOOL);
-        WOOL_BY_COLOR.put(DyeColor.CYAN, Blocks.CYAN_WOOL);
-        WOOL_BY_COLOR.put(DyeColor.PURPLE, Blocks.PURPLE_WOOL);
-        WOOL_BY_COLOR.put(DyeColor.BLUE, Blocks.BLUE_WOOL);
-        WOOL_BY_COLOR.put(DyeColor.BROWN, Blocks.BROWN_WOOL);
-        WOOL_BY_COLOR.put(DyeColor.GREEN, Blocks.GREEN_WOOL);
-        WOOL_BY_COLOR.put(DyeColor.RED, Blocks.RED_WOOL);
-        WOOL_BY_COLOR.put(DyeColor.BLACK, Blocks.BLACK_WOOL);
-    }
+    private static final Map<DyeColor, Block> WOOL_BY_COLOR = new HashMap<>();
 
     public static CushionItem cushion(DyeColor color) {
         return CUSHIONS.get(color).get();
     }
 
     public static Block wool(DyeColor color) {
-        return WOOL_BY_COLOR.get(color);
+        return WOOL_BY_COLOR.computeIfAbsent(color, CBItems::resolveWool);
+    }
+
+    private static Block resolveWool(DyeColor color) {
+        String woolName = color.getName() + "_wool";
+        for (Block block : BuiltInRegistries.BLOCK) {
+            if (BuiltInRegistries.BLOCK.getKey(block).getPath().equals(woolName)) {
+                return block;
+            }
+        }
+        return Blocks.WHITE_WOOL;
     }
 }
--- versions/26.2/common/src/main/java/com/leclowndu93150/cushionbackport/registry/CBItems.java
+++ versions/26.2/common/src/main/java/com/leclowndu93150/cushionbackport/registry/CBItems.java
@@ -1,21 +1,34 @@
 package com.leclowndu93150.cushionbackport.registry;
 
 import com.leclowndu93150.cushionbackport.item.CushionItem;
-import java.util.EnumMap;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Supplier;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 
 public class CBItems {
-    public static final Map<DyeColor, Supplier<CushionItem>> CUSHIONS = new EnumMap<>(DyeColor.class);
+    public static final Map<DyeColor, Supplier<CushionItem>> CUSHIONS = new HashMap<>();
+
+    private static final Map<DyeColor, Block> WOOL_BY_COLOR = new HashMap<>();
 
     public static CushionItem cushion(DyeColor color) {
         return CUSHIONS.get(color).get();
     }
 
     public static Block wool(DyeColor color) {
-        return Blocks.WOOL.pick(color);
+        return WOOL_BY_COLOR.computeIfAbsent(color, CBItems::resolveWool);
+    }
+
+    private static Block resolveWool(DyeColor color) {
+        String woolName = color.getName() + "_wool";
+        for (Block block : BuiltInRegistries.BLOCK) {
+            if (BuiltInRegistries.BLOCK.getKey(block).getPath().equals(woolName)) {
+                return block;
+            }
+        }
+        return Blocks.WOOL.pick(DyeColor.WHITE);
     }
 }
